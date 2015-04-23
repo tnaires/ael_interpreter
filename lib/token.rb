3f8ffaf71152f7e25e7996906bc3667fffd75194
Tarso Aires <tnaires@gmail.com>
2015-04-23 14:37:13 +0530

    Add more token types

--- lib/token.rb
+++ lib/token.rb
@@ -1,9 +1,23 @@
 class Token
   def initialize(char)
+    raise 'Only one character is allowed' unless char.size == 1
+
     @char = char
   end
 
   def operator?
     !/[+\-*\/^%=(),]/.match(@char).nil?
+  end
+
+  def digit?
+    !/[0-9]/.match(@char).nil?
+  end
+
+  def white_space?
+    !/\s/.match(@char).nil?
+  end
+
+  def identifier?
+    !(operator? || digit? || white_space?)
   end
 end
--- spec/token_spec.rb
+++ spec/token_spec.rb
@@ -1,6 +1,12 @@
 require 'token'
 
 RSpec.describe Token do
+  it 'should be created with only one character' do
+    expect {
+      Token.new('abc')
+    }.to raise_error
+  end
+
   describe 'operator?' do
     it 'returns true if the token represents an operator' do
       expect_tokens '+-*/^%=(),', to_be: :operator
@@ -8,6 +14,36 @@ RSpec.describe Token do
 
     it 'returns false if the token is not an operator' do
       expect_tokens 'abcdefgh!@#$', to_not_be: :operator
+    end
+  end
+
+  describe 'digit?' do
+    it 'returns true if the token represents a digit' do
+      expect_tokens '0123456789', to_be: :digit
+    end
+
+    it 'returns false if the token is not a digit' do
+      expect_tokens 'abcdefgh+-*!@#$', to_not_be: :digit
+    end
+  end
+
+  describe 'white_space?' do
+    it 'returns true if the token is a white space' do
+      expect_tokens "\n   \t", to_be: :white_space
+    end
+
+    it 'returns false if the token is not a white space' do
+      expect_tokens 'abcdefgh+-*!@#$', to_not_be: :white_space
+    end
+  end
+
+  describe 'identifier?' do
+    it 'returns true if the token is not an operator, a digit, or a white space' do
+      expect_tokens 'abcdefghijklmn!@#$&', to_be: :identifier
+    end
+
+    it 'returns false if the token is an operator, a digit or a white space' do
+      expect_tokens '1239+-* ', to_not_be: :identifier
     end
   end
 
